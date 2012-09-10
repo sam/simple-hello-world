3b758f0f1aa101dfb1b6739834f25a804fb99a38
Sam Smoot <ssmoot@gmail.com>
2012-09-10 16:25:45 -0500

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,32 @@
+import org.simpleframework.http.core.Container;
+import org.simpleframework.transport.connect.Connection;
+import org.simpleframework.transport.connect.SocketConnection;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.Request;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.io.PrintStream;
+
+public class HelloWorld implements Container {
+
+   public void handle(Request request, Response response) {
+      PrintStream body = response.getPrintStream();
+      long time = System.currentTimeMillis();
+
+      response.set("Content-Type", "text/plain");
+      response.set("Server", "HelloWorld/1.0 (Simple 4.0)");
+      response.setDate("Date", time);
+      response.setDate("Last-Modified", time);
+
+      body.println("Hello World");
+      body.close();
+   } 
+
+   public static void main(String[] list) throws Exception {
+      Container container = new HelloWorld();
+      Connection connection = new SocketConnection(container);
+      SocketAddress address = new InetSocketAddress(8080);
+
+      connection.connect(address);
+   }
+}
